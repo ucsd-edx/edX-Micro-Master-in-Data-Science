fde6b1ddfc8a5a68dc2ac0b4be1e8ff558fc4d2b
yoav freund <yfreund@ucsd.edu>
2017-02-23 14:54:10 -0800

    working on the reveal slides

--- big-data-analytics-using-spark/notebooks/BasicSpark/Reveal/Slides2PDF.py
+++ big-data-analytics-using-spark/notebooks/BasicSpark/Reveal/Slides2PDF.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 import sys,os
-import subprocess
-name=sys.argv[1]
+#import subprocess
+#name=sys.argv[1]
 name='2.\ Execution\ plans\,\ Lazy\ Evaluation\,\ and\ caching.ipynb'
 if name[-6:] != '.ipynb':
     sys.exit('error in filename:'+name)
-command='jupyter nbconvert --to slides %s --reveal-prefix https://cdn.jsdelivr.net/reveal.js/2.6.2 --template=Reveal/slides_reveal_MM.tpl'%name
+command='jupyter nbconvert --to slides %s --reveal-prefix https://cdn.jsdelivr.net/reveal.js/2.6.2 --template=Reveal/slides_reveal_MM.tpl --post serve'%name
 os.system(command)
-server=subprocess.Popen(['python','-m','SimpleHTTPServer','8080'])
-os.system(command)
-command='open %s.slides.html?print-pdf'%name[:-6]
-print command
-os.system(command)
+#server=subprocess.Popen(['python','-m','SimpleHTTPServer','8080'])
+#os.system(command)
+#command='open %s.slides.html?print-pdf'%name[:-6]
+#print command
+#os.system(command)
 
