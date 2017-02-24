4e3b7152d3da4a7c10da660844d3629dd83c942f
ComeOnGetMe <yzp451312410@gmail.com>
2017-02-23 16:36:24 -0800

    toSlides.py and Slides2PDF.py now works perfectly with local reveal.js; cdn version has initialization issue

--- big-data-analytics-using-spark/notebooks/BasicSpark/Reveal/Slides2PDF.py
+++ big-data-analytics-using-spark/notebooks/BasicSpark/Reveal/Slides2PDF.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import sys,os
 #import subprocess
-#name=sys.argv[1]
-name='2.\ Execution\ plans\,\ Lazy\ Evaluation\,\ and\ caching.ipynb'
+name=sys.argv[1]
+# name='2.\ Execution\ plans\,\ Lazy\ Evaluation\,\ and\ caching.ipynb'
 if name[-6:] != '.ipynb':
     sys.exit('error in filename:'+name)
-command='jupyter nbconvert --to slides %s --reveal-prefix https://cdn.jsdelivr.net/reveal.js/2.6.2 --template=Reveal/slides_reveal_MM.tpl --post serve'%name
+# command='jupyter nbconvert --to slides "%s" --reveal-prefix https://cdn.jsdelivr.net/reveal.js/2.6.2 --template=Reveal/slides_reveal_MM.tpl --post serve'%name
+command='jupyter nbconvert --to slides "%s" --template=Reveal/slides_reveal_MM.tpl --post serve'%name
 os.system(command)
 #server=subprocess.Popen(['python','-m','SimpleHTTPServer','8080'])
 #os.system(command)
