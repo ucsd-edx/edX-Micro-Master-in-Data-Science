f78fd681f82ce1c25ae795dd8ca757d71595430c
ComeOnGetMe <yzp451312410@gmail.com>
2017-02-27 14:22:50 -0800

    modified Slides2PDF.py

--- big-data-analytics-using-spark/notebooks/BasicSpark/Reveal/Slides2PDF.py
+++ big-data-analytics-using-spark/notebooks/BasicSpark/Reveal/Slides2PDF.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-import sys,os
-#import subprocess
+import sys,os,webbrowser
+import subprocess,time
+
 name=sys.argv[1]
-# name='2.\ Execution\ plans\,\ Lazy\ Evaluation\,\ and\ caching.ipynb'
 if name[-6:] != '.ipynb':
     sys.exit('error in filename:'+name)
 # command='jupyter nbconvert --to slides "%s" --reveal-prefix https://cdn.jsdelivr.net/reveal.js/2.6.2 --template=Reveal/slides_reveal_MM.tpl --post serve'%name
 command='jupyter nbconvert --to slides "%s" --template=Reveal/slides_reveal_MM.tpl --post serve'%name
-os.system(command)
-#server=subprocess.Popen(['python','-m','SimpleHTTPServer','8080'])
-#os.system(command)
-#command='open %s.slides.html?print-pdf'%name[:-6]
-#print command
-#os.system(command)
+print command
+subprocess.Popen(['jupyter', 'nbconvert', '--to', 'slides', "{}".format(name), '--template=Reveal/slides_reveal_MM.tpl', '--post', 'serve'],
+                 stderr=subprocess.STDOUT)
 
+time.sleep(3)  # wait nbconvert to do the conversion
+html_name = name.replace('ipynb', 'slides.html')
+webbrowser.open('http://127.0.0.1:8000/{}?print-pdf'.format(html_name), new=2)
