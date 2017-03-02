17f9cb4c7fbdeac2a0a2b80f1811ce4583e4d96b
ComeOnGetMe <yzp451312410@gmail.com>
2017-03-01 17:20:51 -0800

    bug fixing

--- big-data-analytics-using-spark/Reveal/toSlide.py
+++ big-data-analytics-using-spark/Reveal/toSlide.py
@@ -6,7 +6,7 @@ def runCommand(command):
 name=sys.argv[1]
 if name[-6:] != '.ipynb':
     sys.exit('error in filename:'+name)
-command='jupyter nbconvert --to slides "%s" --template=Reveal/slides_reveal_MM.tpl --post serve'%name
+command='jupyter nbconvert --to slides "{}" --template={}/slides_reveal_MM.tpl --post serve'.format(name, os.environ['REVEAL_HOME'])
 print 'running command:',command
 os.system(command)
 
