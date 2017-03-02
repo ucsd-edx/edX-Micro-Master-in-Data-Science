32a5c5e7ee8f003284021b3c8e955d572056ff47
ComeOnGetMe <yzp451312410@gmail.com>
2017-03-02 11:25:32 -0800

    setup.sh

--- big-data-analytics-using-spark/Reveal/setup.sh
+++ big-data-analytics-using-spark/Reveal/setup.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
-mkdir -p bin
-cp Slides2PDF.py Slides2PDF
-cp toSlide.py toSlides
-chmod +x Slides2PDF
-chmod +x toSlides
-mv Slides2PDF toSlides bin/
-
-REVEAL_HOME=$PWD
+#!/bin/env bash
+REVEAL_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# echo $REVEAL_HOME
 export REVEAL_HOME
 export PATH=$REVEAL_HOME/bin:$PATH
