070997e18db98b2f4bd2ca308e21c702fec81838
yoav freund <yfreund@ucsd.edu>
2017-03-15 10:24:46 -0700

    working on analysis of small fractions of US_weather file

---
+++ big-data-analytics-using-spark/notebooks/2.PCA/lib/YearPlotter.py
@@ -0,0 +1,20 @@
+from datetime import date
+from numpy import shape
+from matplotlib.dates import MonthLocator, DateFormatter
+class YearPlotter:
+    def __init__(self):
+        start=365*1+1
+        self.dates=[date.fromordinal(i) for i in range(start,start+365)]
+        self.monthsFmt = DateFormatter("%b")
+        self.months = MonthLocator(range(1, 13), bymonthday=1, interval=1)
+    def plot(self,T,fig,ax,title=''):
+        if shape(T)[0] != 365:
+            raise ValueError("First dimension of T should be 365. Shape(T)="+str(shape(T)))
+        ax.plot(self.dates,T);
+        ax.set_title(title)
+        ax.xaxis.set_major_locator(self.months)
+        ax.xaxis.set_major_formatter(self.monthsFmt)
+        # rotate and align the tick labels so they look better
+        fig.autofmt_xdate()
+        ax.grid()
+        
