7c527e83525297ca880f508d4e804a93d4393f91
Ubuntu <ubuntu@ip-172-31-11-199.us-west-2.compute.internal>
2017-04-13 23:41:49 +0000

    reformatting directories

---
+++ big-data-analytics-using-spark/notebooks/1.BasicSpark/Tester/SimpleCount.py
@@ -0,0 +1,15 @@
+import pickle
+
+from basic_tester import *
+
+def exercise0_1(pickleFile, func_student):
+    checkExerciseFromPickle(pickleFile, func_student,TestList,'ex0_1', multiInputs=True)
+    
+def exercise0_2(pickleFile, func_student):
+    checkExerciseFromPickle(pickleFile, func_student,TestList,'ex0_2')
+    
+def exercise0_3(pickleFile, func_student):
+    checkExerciseFromPickle(pickleFile, func_student,TestList,'ex0_3')
+    
+def exercise0_4(pickleFile, func_student):
+    checkExerciseFromPickle(pickleFile, func_student,TestList,'ex0_4', multiInputs=True)
