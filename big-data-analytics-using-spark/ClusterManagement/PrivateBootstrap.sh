e245d8d97f991a0100d29e23f79425f74b60db71
yoav freund <yfreund@ucsd.edu>
2018-02-18 19:01:43 -0800

    debugging privateBootstrap

--- big-data-analytics-using-spark/ClusterManagement/PrivateBootstrap.sh
+++ big-data-analytics-using-spark/ClusterManagement/PrivateBootstrap.sh
@@ -3,24 +3,24 @@ if grep isMaster /mnt/var/lib/info/instance.json | grep true;
 then
    cd /mnt/workspace/
 
-   date +%H.%M:%S:%N  #>> /mnt/workspace/PrivateBootstrap.log
-   echo “Start of bootsrap, set up git” #>> /mnt/workspace/PrivateBootstrap.log
+   date +%H.%M:%S:%N  >> /mnt/workspace/PrivateBootstrap.log
+   echo “Start of bootsrap, set up git” >> /mnt/workspace/PrivateBootstrap.log
    git config --global user.email "yoav.freund@gmail.com"
    git config --global user.name “Yoav Freund”
    git config --global credential.helper cache
    echo "git clone https://github.com/ucsd-edx/edX-Micro-Master-in-Data-Science.git" > clone.sh
 
-   date +%H.%M:%S:%N  #>> /mnt/workspace/PrivateBootstrap.log
-   echo “copy files from S3 to Local”  #>> /mnt/workspace/PrivateBootstrap.log
+   date +%H.%M:%S:%N  >> /mnt/workspace/PrivateBootstrap.log
+   echo “copy files from S3 to Local”  >> /mnt/workspace/PrivateBootstrap.log
    mkdir Data
    cd Data
    aws s3 cp --recursive s3://dse-weather/weather.parquet  ./weather.parquet
 
-   date +%H.%M:%S:%N  #>> /mnt/workspace/PrivateBootstrap.log
-   echo “copy files from Local to HDFS”  #>> /mnt/workspace/PrivateBootstrap.log
-   hadoop fs -mkdir /weather
-   hadoop fs -copyFromLocal weather.parquet /weather/weather.parquet
+   date +%H.%M:%S:%N  >> /mnt/workspace/PrivateBootstrap.log
+   echo “copy files from Local to HDFS”  >> /mnt/workspace/PrivateBootstrap.log
+   hdfs dfs -mkdir /weather
+   hdfs dfs -copyFromLocal weather.parquet /weather/weather.parquet
 
-   date +%H.%M:%S:%N  #>> /mnt/workspace/PrivateBootstrap.log
-   echo “Bootstrap done”  #>> /mnt/workspace/PrivateBootstrap.log
+   date +%H.%M:%S:%N  >> /mnt/workspace/PrivateBootstrap.log
+   echo “Bootstrap done”  >> /mnt/workspace/PrivateBootstrap.log
 fi
