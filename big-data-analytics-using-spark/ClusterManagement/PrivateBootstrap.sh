b0f9f74380c7010731ec902d489eec23d81c36b4
yoav freund <yfreund@ucsd.edu>
2018-02-19 17:06:33 -0800

    debugging Bootstrap

--- big-data-analytics-using-spark/ClusterManagement/PrivateBootstrap.sh
+++ big-data-analytics-using-spark/ClusterManagement/PrivateBootstrap.sh
@@ -3,29 +3,29 @@ if grep isMaster /mnt/var/lib/info/instance.json | grep true;
 then
    cd /mnt/workspace/
 
-   echo '====================Starting======================'  >> /mnt/workspace/PrivateBootstrap.log
+   echo '====================Starting======================'  
    
-   date +%H.%M:%S:%N  >> /mnt/workspace/PrivateBootstrap.log
-   echo “Start of bootsrap, set up git” >> /mnt/workspace/PrivateBootstrap.log
+   date +%H.%M:%S:%N  
+   echo “Start of bootsrap, set up git” 
    git config --global user.email "yoav.freund@gmail.com"
    git config --global user.name “Yoav Freund”
    git config --global credential.helper cache
    echo "git clone https://github.com/ucsd-edx/edX-Micro-Master-in-Data-Science.git" > clone.sh
 
-   date +%H.%M:%S:%N  >> /mnt/workspace/PrivateBootstrap.log
-   echo “copy files from S3 to Local”  >> /mnt/workspace/PrivateBootstrap.log
+   date +%H.%M:%S:%N  
+   echo “copy files from S3 to Local”  
    mkdir Data
    cd Data
    aws s3 cp --recursive s3://dse-weather/weather.parquet  ./weather.parquet
    aws s3 cp --recursive s3://dse-weather/info/stations.parquet/ ./stations.parquet
-   date +%H.%M:%S:%N  >> /mnt/workspace/PrivateBootstrap.log
-   echo “copy files from Local to HDFS”  >> /mnt/workspace/PrivateBootstrap.log
+   date +%H.%M:%S:%N  
+   echo “copy files from Local to HDFS”  
    hdfs dfs -mkdir /weather
    hdfs dfs -copyFromLocal weather.parquet /weather/weather.parquet
    hdfs dfs -copyFromLocal stations.parquet /weather/stations.parquet
 
-   hdfs dfs -ls /weather/  >> /mnt/workspace/PrivateBootstrap.log
+   hdfs dfs -ls /weather/  
 
-   date +%H.%M:%S:%N  >> /mnt/workspace/PrivateBootstrap.log
-   echo “Bootstrap done”  >> /mnt/workspace/PrivateBootstrap.log
+   date +%H.%M:%S:%N  
+   echo “Bootstrap done”  
 fi
