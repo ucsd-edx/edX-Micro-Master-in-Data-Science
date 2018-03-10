2f78df9395faa34bafbfad717902c3d2b18fa57e
yoav freund <yfreund@ucsd.edu>
2018-03-10 11:15:32 -0800

    version with memory latency measured using blocks - std is distorted

--- big-data-analytics-using-spark/notebooks/Section1-Basics/0.MemoryLatency/lib/measureRandomAccess.py
+++ big-data-analytics-using-spark/notebooks/Section1-Basics/0.MemoryLatency/lib/measureRandomAccess.py
@@ -2,46 +2,62 @@ import numpy as np
 from numpy.random import rand
 import time
 
-def measureRandomAccess(size,filename='',k=100000):
+def measureRandomAccess(sz,filename='',k=1000,batch=100):
     """Measure the distribution of random accesses in computer memory.
 
-    :param size: size of memory block.
-    :param filename: a file that is used as an external buffer. If filename=='' then everything is done in memory.
-    :param k: number of times that the experiment is repeated.
-    :returns: (mean,std,T):
-              mean = the mean of T
-              std = the std of T
+    :param sz: size of memory block.
+    :param filename: a file that is used as an external buffer. If filename=='' then everything is done    :param k: number of times that the experiment is repeated.
+    :param batch: The number of locations poked in a single experiment (multiple pokes performed using numpy, rather than python loop)
+    :returns: (_mean,std,T):
+              _mean = the mean of T
+              _std = the std of T
               T = a list the contains the times of all k experiments
     :rtype: tuple
 
     """
-    # Prepare buffer.
-    if filename == '':
-        inmem=True
-        A=bytearray(size)
-    else:
-        inmem=False
+
+    if filename == '':  #Using main memory as storage
+        # Prepare buffer.
+        A=np.zeros(sz,dtype=np.int8)
+
+        # Read and write k*batch times from/to buffer.
+        sum=0; sum2=0
+        T=np.zeros(k)
+        for i in range(k):
+            if (i%100==0): print('\r',i, end=' ')
+            loc=np.int32(rand(batch)*sz)
+            
+            t=time.time()       # ------------------------------------
+            x=A[loc]            # Measured piece, read and write
+            A[loc]=loc
+            d=(time.time()-t)/batch # ------------------------------------
+
+            T[i]=d
+            sum += d
+            sum2 += d*d
+        _mean=sum/k; _vard=(sum2/k)-_mean**2;
+        _std=np.sqrt(_vard*batch)
+
+    else:                   # Using external storage (disk /SSD)
         file=open(filename,'r+')
         
-    # Read and write k times from/to buffer.
-    sum=0; sum2=0
-    T=np.zeros(k)
-    for i in range(k):
-        if (i%10000==0): print('\r',i, end=' ')
-        t=time.time()
-        loc=int(rand()*size)
-        if inmem:
-            x=A[loc:loc+4]
-            A[loc]=(i % 256)
-        else:
-            file.seek(loc)
+        # Read and write k times from/to buffer.
+        sum=0; sum2=0
+        T=np.zeros(k)
+        for i in range(k):
+            if (i%10000==0): print('\r',i, end=' ')
+            loc=int(rand()*sz)
+
+            t=time.time()       # ------------------------------------
+            file.seek(loc)      #  Measured piece, seek read and write
             poke=file.read(1)
-            file.write("test")
-        d=time.time()-t
-        T[i]=d
-        sum += d
-        sum2 += d*d
-    mean=sum/k; var=(sum2/k)-mean**2; std=np.sqrt(var)
-    if not inmem:
+            file.write("X")
+            d=time.time()-t     # ------------------------------------
+
+            T[i]=d
+            sum += d
+            sum2 += d*d
+        _mean=sum/k; _var=(sum2/k)-_mean**2; _std=np.sqrt(_var)
+        
         file.close()
-    return (mean,std,T)
+    return (_mean,_std,T)
