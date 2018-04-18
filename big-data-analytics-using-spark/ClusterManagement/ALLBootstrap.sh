1c442bc722a0630523e782976195bf333b9749ad
yoav freund <yfreund@ucsd.edu>
2018-04-18 08:45:49 -0700

    Working on installing python libraries on workers, Julaitis solution

--- big-data-analytics-using-spark/ClusterManagement/ALLBootstrap.sh
+++ big-data-analytics-using-spark/ClusterManagement/ALLBootstrap.sh
@@ -1,8 +1,14 @@
 #This script is to be executed on all nodes, both master and workers
 
 echo '====================General Bootstrap Starting======================'  
+echo $PATH
+env
+#/mnt/anaconda/bin/conda install astropy
 
-conda install astropy
+sudo easy_install-3.4 pip
+sudo pip-3.4 install --upgrade pip
+# export PATH="/usr/local/bin:$PATH"
+sudo pip-3.4 install astropy
 
 #add some useful commands to the end of .bashrc
 echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
