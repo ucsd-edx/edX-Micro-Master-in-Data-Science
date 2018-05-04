bc5a060cfebcc5148e4c5c555100896cbd6b41cd
Julaiti Alafate <jalafate@gmail.com>
2018-05-04 09:33:12 -0700

    fixed a typo

--- big-data-analytics-using-spark/notebooks/HW2018/hw2/deploy/RunFromTerminal.sh
+++ big-data-analytics-using-spark/notebooks/HW2018/hw2/deploy/RunFromTerminal.sh
@@ -5,7 +5,7 @@ echo “copy files from S3 to HDFS”
 /usr/bin/hdfs dfs -cp s3://mas-dse-open/Twitter/hw2-files-10mb.txt /twitter
 /usr/bin/hdfs dfs -cp s3://mas-dse-open/Twitter/hw2-files-1gb.txt /twitter
 /usr/bin/hdfs dfs -cp s3://mas-dse-open/Twitter/hw2-files-10gb.txt /twitter
-/usr/bin/hdfs dfs -cp s3://mas-dse-open/Twitter/hw2-files-100gb.txt /twitter
+/usr/bin/hdfs dfs -cp s3://mas-dse-open/Twitter/hw2-files-20gb.txt /twitter
 
 date +%H.%M:%S:%N  
 echo "RunFromTerminal done"
